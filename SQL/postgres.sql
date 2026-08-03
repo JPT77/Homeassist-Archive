51d11a27c37b0e23d06dab572c4339b5edff5f9c
JPT <j-p-t@gmx.net>
2026-08-03 22:39:44 +0200

    added permissions

--- SQL/postgres.sql
+++ SQL/postgres.sql
@@ -43,6 +43,10 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.statistics_meta
     OWNER to postgres;
 
+REVOKE ALL ON TABLE public.statistics_meta FROM homeassist;
+GRANT INSERT, SELECT, UPDATE ON TABLE public.statistics_meta TO homeassist;
+GRANT ALL ON TABLE public.statistics_meta TO postgres;
+
 COMMENT ON TABLE public.statistics_meta
     IS 'Metadata for Home Assistant statistics.';
 
@@ -68,6 +72,37 @@ CREATE TABLE IF NOT EXISTS public.statistics_short_term
 )
 
 TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.statistics_short_term
+    OWNER to postgres;
+
+REVOKE ALL ON TABLE public.statistics_short_term FROM homeassist;
+GRANT INSERT, SELECT, UPDATE ON TABLE public.statistics_short_term TO homeassist;
+GRANT ALL ON TABLE public.statistics_short_term TO postgres;
+
+COMMENT ON TABLE public.statistics_short_term
+    IS '5-minute aggregated Home Assistant statistics.';
+
+-- Index: idx_statistics_metadata
+-- DROP INDEX IF EXISTS public.idx_statistics_metadata;
+CREATE INDEX IF NOT EXISTS idx_statistics_metadata
+    ON public.statistics_short_term USING btree
+    (metadata_id ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+-- Index: idx_statistics_metadata_start
+-- DROP INDEX IF EXISTS public.idx_statistics_metadata_start;
+CREATE INDEX IF NOT EXISTS idx_statistics_metadata_start
+    ON public.statistics_short_term USING btree
+    (metadata_id ASC NULLS LAST, start_at ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+-- Index: idx_statistics_start_at
+-- DROP INDEX IF EXISTS public.idx_statistics_start_at;
+CREATE INDEX IF NOT EXISTS idx_statistics_start_at
+    ON public.statistics_short_term USING btree
+    (start_at ASC NULLS LAST)
+    TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.statistics_short_term
     OWNER to postgres;
@@ -110,3 +145,7 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.import_history
     OWNER to postgres;
+
+REVOKE ALL ON TABLE public.import_history FROM homeassist;
+GRANT INSERT, SELECT, UPDATE ON TABLE public.import_history TO homeassist;
+GRANT ALL ON TABLE public.import_history TO postgres;
